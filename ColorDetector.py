b17175a34e259df50acd7d23b496e8e3667fff7d
J8ckholly <jholly01@villanova.edu>
2023-12-10 22:18:13 -0500

    Final Version of our team's code

---
+++ ColorDetector.py
@@ -0,0 +1,133 @@
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDRaisedButton
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+import cv2
+#from utils import getlimits
+from PIL import Image as img
+from kivy.uix.button import Button 
+from kivy.uix.gridlayout import GridLayout
+from kivy.app import App 
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
+from kivy.lang import Builder
+
+
+
+class BoxLayoutExample(BoxLayout):
+     pass
+
+class WidgetsExample(GridLayout): 
+     my_text = StringProperty("Hello!")   
+     def on_button_click(self):
+          print("button clicked")
+          self.my_text = "You clicked"
+
+class ColorApp(MDApp):
+    #kv = Builder.load_file("SignUpWindow.kv")
+    
+    def build(self):
+        #def __init__():
+        self.colorLower = (29,86,6)
+        self.colorUpper = (64, 255, 255)
+
+        def change_color_to_red(instance):
+            self.colorLower = (160, 100, 100)
+            self.colorUpper = (180, 255, 255)
+
+        def change_color_to_yellow(instance):
+            self.colorLower = (29,86,6)
+            self.colorUpper = (64, 255, 255)
+
+        def change_color_to_green(instance):
+            self.colorLower = (50, 100, 100)
+            self.colorUpper = (70, 255, 255)
+
+        layout = MDBoxLayout(orientation='vertical')
+        red = Button(
+            text="Detect Red",
+            #pos_hint={'center_x':.5, 'center_y':.5},
+            size_hint=(.2,.2),
+        )
+        red.bind(on_press=change_color_to_red)
+
+        green = Button(
+            text="Detect Green",
+            #pos_hint={'center_x':.25, 'center_y':.5},
+            size_hint=(.2,.2),
+        )
+        green.bind(on_press=change_color_to_green)
+
+        yellow = Button(
+            text="Detect Yellow",
+            #pos_hint={'center_x':.75, 'center_y':.5},
+            size_hint=(.2,.2),
+        )
+        yellow.bind(on_press=change_color_to_yellow)
+
+        layout.add_widget(red)
+        layout.add_widget(green)
+        layout.add_widget(yellow)    
+
+
+        self.image = Image()
+        layout.add_widget(self.image)
+        layout.add_widget(MDRaisedButton(
+            text="Yellow Detection Apps",
+            pos_hint={'center_x':.5, 'center_y':.5},
+            size_hint=(None,None)
+        ))
+        
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.load_video, 1/60)
+        return layout
+    
+    def load_video(self, *args):
+        ret, frame = self.capture.read()
+        #Frame initialize
+        self.image_frame = frame
+
+        #color detection
+        blurred = cv2.GaussianBlur(frame, (11,11),0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+
+        #ValidgreencolorLower = (50, 100, 100)
+        #ValidgreencolorUpper = (70, 255, 255)
+        colorLower = self.colorLower
+        colorUpper = self.colorUpper
+        #ValidRedcolorLower = (160, 100, 100)
+        #ValidRedcolorUpper = (180, 255, 255)
+
+        #ValidYellowcolorLower = (29,86,6)
+        #ValidYellowcolorUpper = (64, 255, 255)
+        mask = cv2.inRange(hsv,colorLower,colorUpper)
+        mask = cv2.erode(mask, None, iterations=3)
+        mask = cv2.dilate(mask, None, iterations=3)
+
+        mask_ = img.fromarray(mask)
+        bbox = mask_.getbbox()
+
+        if bbox is not None:
+            x1,y1, x2, y2 = bbox
+            frame = cv2.rectangle(frame, (x1,y1), (x2, y2), (0,255,0), 5)
+
+        buffer = cv2.flip(frame,0).tostring()
+        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+        texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
+        self.image.texture = texture
+
+        
+       
+if __name__ == '__main__':
+    ColorApp().run()
+
+
+
+
+
+
+    
--- main.py
+++ main.py
@@ -1,83 +1,30 @@
-from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.button import MDRaisedButton
-from kivy.uix.image import Image
-from kivy.clock import Clock
-from kivy.graphics.texture import Texture
-import cv2
-#from utils import getlimits
-from PIL import Image as img
-from kivy.uix.button import Button 
-from kivy.uix.gridlayout import GridLayout
-from kivy.app import App 
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
+# Fitness App Main Code
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager
+from HomeScreen import LoginPage
+from kivy.properties import BooleanProperty
 
-class BoxLayoutExample(BoxLayout):
-     pass
 
-class WidgetsExample(GridLayout): 
-     my_text = StringProperty("Hello!")   
-     def on_button_click(self):
-          print("button clicked")
-          self.my_text = "You clicked"
+class WindowManager(ScreenManager):  # creating a class that deals with screen management
+    pass
 
-class MainApp(MDApp):
 
+sm = WindowManager()  # setting a variable to the window manager class
+
+screens = [LoginPage(name="Login_Page")]  # setting an array of lists
+for screen in screens:  # going through all of the screens
+    sm.add_widget(screen)  # adding the screen widget to each
+
+sm.current = "Login_Page"  # setting current window to login
+
+
+class ColorDetectApp(App):  # the class that runs the app
     def build(self):
-        layout = MDBoxLayout(orientation='vertical')
-        layout.add_widget(Button(
-            text="Color Detection",
-            pos_hint={'center_x':.5, 'center_y':.5},
-            size_hint=(.2,.2)
-        ))
-        self.image = Image()
-        layout.add_widget(self.image)
-        layout.add_widget(MDRaisedButton(
-            text="Yellow Detection Apps",
-            pos_hint={'center_x':.5, 'center_y':.5},
-            size_hint=(None,None)
-        ))
-        self.capture = cv2.VideoCapture(0)
-        Clock.schedule_interval(self.load_video, 1/60)
-        return layout
-    
-    def load_video(self, *args):
-        ret, frame = self.capture.read()
-        #Frame initialize
-        self.image_frame = frame
-
-        #color detection
-        blurred = cv2.GaussianBlur(frame, (11,11),0)
-        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-        colorLower = (29,86,6)
-        colorUpper = (64, 255, 255)
-
-        mask = cv2.inRange(hsv,colorLower,colorUpper)
-        mask = cv2.erode(mask, None, iterations=3)
-        mask = cv2.dilate(mask, None, iterations=3)
-
-        mask_ = img.fromarray(mask)
-        bbox = mask_.getbbox()
-
-        if bbox is not None:
-            x1,y1, x2, y2 = bbox
-            frame = cv2.rectangle(frame, (x1,y1), (x2, y2), (0,255,0), 5)
-
-        buffer = cv2.flip(frame,0).tostring()
-        texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-        texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
-        self.image.texture = texture
-        
-       
-if __name__ == '__main__':
-    MainApp().run()
+        App.title = "Color Detecting App"  # setting the app title
+        return sm  # running the app by returning the current window
 
 
-
-
-
-
-    
+if __name__ == '__main__':  # The value of __name__ attribute is set to “__main__” when module is run as main program
+    ColorDetectApp = ColorDetectApp()
+    ColorDetectApp.run()
